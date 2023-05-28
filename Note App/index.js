f3e60eb3b5a41987956cfacedf29d35838aeaae4
isfnev <negi88412@gmail.com>
2023-05-28 22:00:44 +0530

    rewrite javascript for practicing the Note App function

--- Note App/index.js
+++ Note App/index.js
@@ -1,25 +1,11 @@
 const btnEl = document.getElementById('btn');
-const appEl = document.getElementById('app');
-const noteEls = document.querySelectorAll('.note');
 
-noteEls.forEach((noteEl)=>{
-    noteEl.addEventListener('dbclick', ()=>{
-        // remove this element
-
-        // remove the data of this element from local storage
-
-    });
-});
-
-function addNote()
-{
-    const newNote = document.createElement('textarea');
-    newNote.classList.add('note');
-    newNote.placeholder='Enter Note';
-    newNote.rows='10';
-    newNote.cols='30';
-
-    appEl.insertBefore(newNote, btnEl);
+const addNote = function(){
+	const noteObj = {
+		id:Math.floor(Math.random()*100000),
+		content:'',
+	}
+	
 }
 
 btnEl.addEventListener('click', addNote);
