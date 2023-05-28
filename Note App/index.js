c4bed01d9052b01eebfcb578de9783c4f305c9de
isfnev <negi88412@gmail.com>
2023-05-28 22:28:05 +0530

    committing early because ujjwal can delete by mistake

--- Note App/index.js
+++ Note App/index.js
@@ -1,11 +1,28 @@
 const btnEl = document.getElementById('btn');
+const appEl = document.getElementById('app');
+
+const createNoteNode = ()=>{
+	const element = document.createElement('textarea');
+	element.classList.add('note');
+	element.placeholder = 'Enter Note' ;
+
+	return element;
+}
 
 const addNote = function(){
 	const noteObj = {
 		id:Math.floor(Math.random()*100000),
 		content:'',
 	}
-	
+	const newEl = createNoteNode() ;
+
+	newEl.addEventListener('keyup', (event)=>{
+		console.log(event.target.value);
+	});
+
+	appEl.insertBefore(newEl, btnEl);
 }
+
+addNote();
 
 btnEl.addEventListener('click', addNote);
