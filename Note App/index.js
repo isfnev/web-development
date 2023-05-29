ba903321615108f64afab50726f70e9980122f72
isfnev <negi88412@gmail.com>
2023-05-29 18:21:12 +0530

    practice

--- Note App/index.js
+++ Note App/index.js
@@ -1,28 +1,23 @@
 const btnEl = document.getElementById('btn');
 const appEl = document.getElementById('app');
 
-const createNoteNode = ()=>{
-	const element = document.createElement('textarea');
-	element.classList.add('note');
-	element.placeholder = 'Enter Note' ;
-
-	return element;
+const createNoteElement = function(){
+    const element = document.createElement('textarea');
+    element.classList.add('note');
+    element.placeholder = 'Enter Note';
+    console.log('clicked');
+    
+    return element;
 }
 
-const addNote = function(){
-	const noteObj = {
-		id:Math.floor(Math.random()*100000),
-		content:'',
-	}
-	const newEl = createNoteNode() ;
-
-	newEl.addEventListener('keyup', (event)=>{
-		console.log(event.target.value);
-	});
-
-	appEl.insertBefore(newEl, btnEl);
+const addNote = ()=>{
+    const noteObj = {
+        id:Math.floor(Math.random()*100000),
+        content:'',
+    };
+    console.log('click');
+    const noteEl = createNoteElement();
+    appEl.insertBefore(noteEl, btnEl);
 }
-
-addNote();
 
 btnEl.addEventListener('click', addNote);
